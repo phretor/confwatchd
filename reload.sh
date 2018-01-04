930e3437bbae283472b8ee32255092850a8d14c7
evilsocket <evilsocket@gmail.com>
2018-01-04 15:21:08 +0100

    fix

--- reload.sh
+++ reload.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git pull && make && make setcap && clear && ./confwatchd -config prod-config.json.json -seed $HOME/confwatch-data && ./confwatchd -config prod-config.json
+git pull && make && make setcap && clear && ./confwatchd -config prod-config.json -seed $HOME/confwatch-data && ./confwatchd -config prod-config.json
