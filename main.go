07cae03ba221274a2bd09b57d97fcab878b8a89a
evilsocket <evilsocket@gmail.com>
2018-01-06 11:49:02 +0100

    fix: adding connection close middleware to fix too many open files issues.

--- main.go
+++ main.go
@@ -141,6 +141,11 @@ func main() {
 	router.HTMLRender = render.Init()
 	router.Use(middlewares.Security())
 	router.Use(middlewares.ServeStatic("/", "static", "index.html"))
+	// make sure connection is closed after every request is routed
+	router.Use(func(c *gin.Context) {
+		c.Request.Header.Set("Connection", "close")
+		c.Next()
+	})
 
 	router.GET("/sitemap-index.xml", controllers.IndexSitemap)
 	router.GET("/sitemap-categories.xml", controllers.CategorySitemap)
