2e3add6b61e03649ed9fc448eac25f5b95a9e4e8
evilsocket <evilsocket@gmail.com>
2018-01-10 06:47:26 +0100

    fix: fixed twitter bot sync > async

--- jobs/twitter_bot.go
+++ jobs/twitter_bot.go
@@ -24,62 +24,64 @@ func StartTwitterBot() error {
 		return err
 	}
 
-	log.Infof("Twitter bot started, last tweet: %s", log.Dim(feed[0].Text))
+	go func() {
+		log.Infof("Twitter bot started, last tweet: %s", log.Dim(feed[0].Text))
 
-	for {
-		log.Debugf("Twitter bot loop.")
-		found, edition := models.FirstEditionToShare(1)
-		if found == true {
-			log.Infof("Sharing %s ...", edition.Title)
+		for {
+			log.Debugf("Twitter bot loop.")
+			found, edition := models.FirstEditionToShare(1)
+			if found == true {
+				log.Infof("Sharing %s ...", edition.Title)
 
-			now := time.Now()
-			msg := ""
-			toStart := edition.Starts.Sub(now)
-			will := edition.Starts.After(now)
-			t := humanize.RelTime(edition.Starts, now, "ago", "from now")
+				now := time.Now()
+				msg := ""
+				toStart := edition.Starts.Sub(now)
+				will := edition.Starts.After(now)
+				t := humanize.RelTime(edition.Starts, now, "ago", "from now")
 
-			// Never been shared before.
-			if edition.SharedAt.IsZero() == true {
-				msg = fmt.Sprintf("Hey, check this out, %s has been added to the database, it will start in %s! %s",
-					edition.Title,
-					t,
-					fmt.Sprintf("https://confwatch.ninja/events/%s/editions/%s",
-						edition.Event().Slug,
-						edition.Slug,
-					),
-				)
-			} else if will && toStart.Hours() < 24 {
-				msg = fmt.Sprintf("Yo, %s is starting today! %s",
-					edition.Title,
-					fmt.Sprintf("https://confwatch.ninja/events/%s/editions/%s",
-						edition.Event().Slug,
-						edition.Slug,
-					),
-				)
-			} else if will && toStart.Hours() < 48 {
-				msg = fmt.Sprintf("Hey, %s is starting tomorrow :D %s",
-					edition.Title,
-					fmt.Sprintf("https://confwatch.ninja/events/%s/editions/%s",
-						edition.Event().Slug,
-						edition.Slug,
-					),
-				)
-			}
-
-			if msg != "" {
-				log.Infof("Tweeting: %s", log.Dim(msg))
-				_, _, err := client.Statuses.Update(msg, nil)
-				if err != nil {
-					log.Errorf("Error while sending tweet: %s", err)
+				// Never been shared before.
+				if edition.SharedAt.IsZero() == true {
+					msg = fmt.Sprintf("Hey, check this out, %s has been added to the database, it will start in %s! %s",
+						edition.Title,
+						t,
+						fmt.Sprintf("https://confwatch.ninja/events/%s/editions/%s",
+							edition.Event().Slug,
+							edition.Slug,
+						),
+					)
+				} else if will && toStart.Hours() < 24 {
+					msg = fmt.Sprintf("Yo, %s is starting today! %s",
+						edition.Title,
+						fmt.Sprintf("https://confwatch.ninja/events/%s/editions/%s",
+							edition.Event().Slug,
+							edition.Slug,
+						),
+					)
+				} else if will && toStart.Hours() < 48 {
+					msg = fmt.Sprintf("Hey, %s is starting tomorrow :D %s",
+						edition.Title,
+						fmt.Sprintf("https://confwatch.ninja/events/%s/editions/%s",
+							edition.Event().Slug,
+							edition.Slug,
+						),
+					)
 				}
+
+				if msg != "" {
+					log.Infof("Tweeting: %s", log.Dim(msg))
+					_, _, err := client.Statuses.Update(msg, nil)
+					if err != nil {
+						log.Errorf("Error while sending tweet: %s", err)
+					}
+				}
+
+				edition.SharedAt = time.Now()
+				models.Save(&edition)
 			}
 
-			edition.SharedAt = time.Now()
-			models.Save(&edition)
+			time.Sleep(time.Hour * 1)
 		}
-
-		time.Sleep(time.Hour * 1)
-	}
+	}()
 
 	return nil
 }
